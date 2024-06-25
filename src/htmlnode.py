0f419052eb08e18f5eee1f75f64611b78b3d66e4
FeliciaFredlund <felicia.fredlund@gmail.com>
2024-06-25 18:12:14 +0200

    Extended HTMLNode with LeafNode and ParentNode, plus tests for it all

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,12 +1,18 @@
 from enum import Enum
 
 class Tag(Enum):
-    P = "p"
+    HEAD = "head"
+    BODY = "body"
     DIV = "div"
-    A = "a"
-    H1 = "h1"
-    H2 = "h2"
-    H3 = "h3"
+    PARAGRAPH = "p"
+    SPAN = "span"
+    HEADING1 = "h1"
+    HEADING2 = "h2"
+    HEADING3 = "h3"
+    LINK = "a"
+    BOLD = "b"
+    ITALIC = "i"
+    NONE = None
 
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
@@ -15,14 +21,63 @@ class HTMLNode:
         self.children = children    # A list of HTMLnode objects representing the children of this node
         self.props = props          # A dictionary holding the attributes of the tag, example {"href": "https://www.test.com"}
 
-    def to_html(self):
+    def to_html(self):          #creates a string with correct HTML for all elements
         raise NotImplementedError("to_html method not implemented")
     
     def props_to_html(self):
-        attributes = []
-        for key, value in self.props.items():
-            attributes.append(f' {key}="{value}"')
-        return "".join(attributes)      # if self.props is None/empty, an empty string is sent
+        if self.props is None:
+            return ""
+        
+        string_props = []
+        for key, value in self.props.items():       # changes each prop to a string (starting with a space) with correct HTML
+            string_props.append(f' {key}="{value}"')
+        return "".join(string_props)
     
     def __repr__(self):
-        return f"HTMLNode(tag: {self.tag.value}, value: {self.value}, children: {self.children}, props/attributes: {self.props})"
+        return f"HTMLNode(tag: {self.tag.value}, value: {self.value}, children: {self.children}, props: {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):          #creates a string with correct HTML for all elements
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        
+        if self.tag.value is None:
+            return self.value
+        
+        return f"<{self.tag.value}{self.props_to_html()}>{self.value}</{self.tag.value}>"
+
+    def __repr__(self):
+        return f"LeafNode(tag: {self.tag.value}, value: {self.value}, props: {self.props})"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):          #creates a string with correct HTML for all elements
+        if self.tag is Tag.NONE:
+            raise ValueError("Invalid HTML: no tag")
+
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+        
+        strings_for_parent_and_children = []
+        # Parent opening tag
+        strings_for_parent_and_children.append(f"<{self.tag.value}{self.props_to_html()}>")
+        
+        # Adding all children strings
+        for child in self.children:
+            strings_for_parent_and_children.append(child.to_html())
+        
+        # Parent closing tag
+        strings_for_parent_and_children.append(f"</{self.tag.value}>")
+
+        return "".join(strings_for_parent_and_children)
+
+    
+    def __repr__(self):
+        return f"ParentNode(tag: {self.tag.value}, children: {self.children}, props: {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,25 +2,135 @@ import unittest
 
 from htmlnode import (
     HTMLNode,
+    LeafNode,
+    ParentNode,
     Tag
 )
 
 class TestHTMLNode(unittest.TestCase):    
     def test_props_to_html(self):
-        node = HTMLNode(Tag.A, "link", None, {"id": "title", "href": "https://test.com", "target": "_blank"})
+        node = HTMLNode(Tag.LINK, "link", None, {"id": "title", "href": "https://test.com", "target": "_blank"})
         self.assertEqual(
             node.props_to_html(),
             ' id="title" href="https://test.com" target="_blank"'
         )
 
     def test_repr(self):
-        node = HTMLNode(Tag.H1, "This is a heading", None, {"id": "title"})
+        node = HTMLNode(Tag.HEADING1, "This is a heading", None, {"id": "title"})
         self.assertEqual(
             repr(node),
-            "HTMLNode(tag: h1, value: This is a heading, children: None, props/attributes: {'id': 'title'})"
+            "HTMLNode(tag: h1, value: This is a heading, children: None, props: {'id': 'title'})"
+        )
+
+    def test_repr_no_tag(self):
+        node = HTMLNode(None, "This is a heading", None, {"id": "title"})
+        self.assertEqual(
+            repr(node),
+            "HTMLNode(tag: None, value: This is a heading, children: None, props: {'id': 'title'})"
         )
 
 
+class TestLeafNode(unittest.TestCase): 
+    def test_leaf_node_to_html(self):
+        leaf_node = LeafNode(Tag.LINK, "This is a leaf node", {"href": "https://test.com", "target": "_blank"})
+        self.assertEqual(
+            leaf_node.to_html(),
+            '<a href="https://test.com" target="_blank">This is a leaf node</a>'
+        )
+
+    def test_leaf_node_to_html_no_tag(self):
+        leaf_node = LeafNode(None, "This is another leaf node")
+        self.assertEqual(
+            leaf_node.to_html(),
+            "This is another leaf node"
+        )
+
+    def test_leaf_node_repr(self):
+        leaf_node = LeafNode(Tag.LINK, "This is a leaf node", {"href": "https://test.com", "target": "_blank"})
+        self.assertEqual(
+            repr(leaf_node),
+            "LeafNode(tag: a, value: This is a leaf node, props: {'href': 'https://test.com', 'target': '_blank'})"
+        )
+
+
+class TestParentNode(unittest.TestCase):
+    def test_parent_node_to_html_1(self):
+        # Testing a parent node with only leaf nodes
+        parent_node = ParentNode(
+            Tag.PARAGRAPH,
+            [
+                LeafNode(None, "leaf node"),
+                LeafNode(Tag.LINK, "This is a test", {"href": "https://test.com"})
+            ],
+            {"id": "title"}
+        )
+        self.assertEqual(
+            parent_node.to_html(),
+            '<p id="title">leaf node<a href="https://test.com">This is a test</a></p>'
+        )
+
+    def test_parent_node_to_html_2(self):
+        # Testing a parent node with parent nodes and leaf nodes
+        parent_node = ParentNode(
+            Tag.DIV,
+            [
+                ParentNode(
+                    Tag.PARAGRAPH,
+                    [LeafNode(Tag.ITALIC, "This is a test of "), LeafNode(Tag.BOLD, "a leaf node")],
+                    {"id": "first parent"}
+                ),
+                ParentNode(
+                    Tag.PARAGRAPH,
+                    [LeafNode(None, "and parent node")],
+                    {"id": "second parent"}
+                ),
+                LeafNode(None, "This is the leaf node!")
+            ],
+            {"id": "main"}
+        )
+        self.assertEqual(
+            parent_node.to_html(),
+            '<div id="main"><p id="first parent"><i>This is a test of </i><b>a leaf node</b></p><p id="second parent">and parent node</p>This is the leaf node!</div>'
+        )
+
+    def test_parent_node_to_html_3(self):
+        # Testing parent nodes in parent nodes in parent nodes
+        parent_node = ParentNode(
+            Tag.BODY,
+            [
+                ParentNode(
+                    Tag.DIV,
+                    [
+                        ParentNode(
+                            Tag.PARAGRAPH,
+                            [LeafNode(None, "This is the leaf node!")],
+                            {"id": "third parent"}
+                        )
+                    ],
+                    {"id": "second parent"}
+                )
+            ],
+            {"id": "first parent"}
+        )
+        self.assertEqual(
+            parent_node.to_html(),
+            '<body id="first parent"><div id="second parent"><p id="third parent">This is the leaf node!</p></div></body>'
+        )
+
+    
+    def test_parent_node_repr(self):
+        parent_node = ParentNode(
+            Tag.PARAGRAPH,
+            [
+                LeafNode(None, "leaf node"),
+                LeafNode(Tag.LINK, "This is a test", {"href": "https://test.com"})
+            ],
+            {"id": "title"}
+        )
+        self.assertEqual(
+            repr(parent_node),
+            "ParentNode(tag: p, children: [LeafNode(tag: None, value: leaf node, props: None), LeafNode(tag: a, value: This is a test, props: {'href': 'https://test.com'})], props: {'id': 'title'})"
+        )
 
 
 if __name__ == "__main__":
