e110e984766bc89bc42b01325ac195850cfc5d74
FeliciaFredlund <felicia.fredlund@gmail.com>
2025-05-09 14:54:22 +0200

    Fixed italics.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -11,7 +11,7 @@ def text_to_textnodes(text):
 
     #bold & italic & code
     nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
-    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
     nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
 
     #images & links
